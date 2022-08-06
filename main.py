3a49f9b2b9adee46d1c3e089f796d0992de860e6
Mohd Mohtashim <mohtashim0007@gmail.com>
2022-08-06 11:43:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+%%writefile app.py
+
 import streamlit as st
 import sklearn
 import pickle
@@ -56,23 +59,23 @@ elif radio == 'Use Custom Data':
 
   #NOrmalization method X_new = (X - X_min)/(X_max - X_min)
   with col1:
-    Glucose = st.number_input('Glucose :',  df['Glucose'].min(), df['Glucose'].max(), value = df['Glucose'].mean())
+    Glucose = st.number_input('Glucose :',  df['Glucose'].min(), df['Glucose'].max(), value = df['Glucose'].mean(), step = 3.4)
     Glucose = (Glucose - df['Glucose'].min()) / (df['Glucose'].max() - df['Glucose'].min())
-    BloodPressure = st.number_input('BloodPressure :',  df['BloodPressure'].min(), df['BloodPressure'].max(), value = df['BloodPressure'].mean())
+    BloodPressure = st.number_input('BloodPressure :',  df['BloodPressure'].min(), df['BloodPressure'].max(), value = df['BloodPressure'].mean(), step = 3.4)
     BloodPressure = (BloodPressure - df['BloodPressure'].min()) / (df['BloodPressure'].max() - df['BloodPressure'].min())
-    SkinThickness = st.number_input('SkinThickness :',  df['SkinThickness'].min(), df['SkinThickness'].max(), value = df['SkinThickness'].mean())
+    SkinThickness = st.number_input('SkinThickness :',  df['SkinThickness'].min(), df['SkinThickness'].max(), value = df['SkinThickness'].mean(), step = 3.4)
     SkinThickness = (SkinThickness - df['SkinThickness'].min()) / (df['SkinThickness'].max() - df['SkinThickness'].min())
-    Insulin = st.number_input('Insulin :',  df['Insulin'].min(), df['Insulin'].max(), value = df['Insulin'].mean())
+    Insulin = st.number_input('Insulin :',  df['Insulin'].min(), df['Insulin'].max(), value = df['Insulin'].mean(), step = 3.4)
     Insulin = (Insulin - df['Insulin'].min()) / (df['Insulin'].max() - df['Insulin'].min())
 
   with col2:
-    BMI = st.number_input('BMI :',  df['BMI'].min(), df['BMI'].max(), value = df['BMI'].mean())
+    BMI = st.number_input('BMI :',  df['BMI'].min(), df['BMI'].max(), value = df['BMI'].mean(), step = 3.4)
     BMI = (BMI - df['BMI'].min()) / (df['BMI'].max() - df['BMI'].min())
-    Age = st.number_input('Age :',  df['Age'].min(), df['Age'].max(), value = 27)
+    Age = st.number_input('Age :',  df['Age'].min(), df['Age'].max(), value = 27, step = 4)
     Age = (Age - df['Age'].min()) / (df['Age'].max() - df['Age'].min())
-    DiabetesPedigreeFunction = st.number_input('DiabetesPedigreeFunction :',  df['DiabetesPedigreeFunction'].min(), df['DiabetesPedigreeFunction'].max(), value = df['DiabetesPedigreeFunction'].mean())
+    DiabetesPedigreeFunction = st.number_input('DiabetesPedigreeFunction :',  df['DiabetesPedigreeFunction'].min(), df['DiabetesPedigreeFunction'].max(), value = df['DiabetesPedigreeFunction'].mean() , step = 3.4)
     DiabetesPedigreeFunction = (DiabetesPedigreeFunction - df['DiabetesPedigreeFunction'].min()) / (df['DiabetesPedigreeFunction'].max() - df['DiabetesPedigreeFunction'].min())
-    Pregnancies = st.number_input('Pregnancies :',  df['Pregnancies'].min(), 9, value = 0)
+    Pregnancies = st.number_input('Pregnancies :',  df['Pregnancies'].min(), 9, value = 0,  step = 1)
     Pregnancies = (Pregnancies - df['Pregnancies'].min()) / (df['Pregnancies'].max() - df['Pregnancies'].min())
 
   bt = st.button('Submit')
@@ -82,3 +85,5 @@ elif radio == 'Use Custom Data':
     st.write('Random Forest Predicted : ', isDiabetic(model.predict([vals])))
     st.write('SVM Predicted : ', isDiabetic(model2.predict([vals])))
     
+
+
